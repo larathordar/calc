f6833754329fcc7c759f3656d29d0f3d71990891
Lara Thordardottir <lara-thordar@hotmail.com>
2017-12-12 21:08:01 +0100

    name and numbers

--- calc.rb
+++ calc.rb
@@ -1,5 +1,11 @@
-puts "hello world"
-puts""
-puts "goodbye"
-puts "I like " + "apple pie"
-puts "blink " *4
+puts "Hello, what's your first name?"
+first = gets .chomp
+puts "What's your middle name?"
+second = gets .chomp
+puts "What's your last name"
+last = gets .chomp
+puts "Hello " + first + " " + second + " " + last +  " nice to meet you."
+puts "Hello, what's your favorite number"
+number = gets .chomp
+hey = 1 + number.to_i
+puts "the number " + hey.to_s + " is a bigger and better number".chomp
